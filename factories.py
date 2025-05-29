93c2a5a449ca1f7f8db1085bf9586622eb5a40d3
MourZ <zemourkhedi@yahoo.fr>
2025-05-29 18:55:32 +0200

    Utils Factories

---
+++ factories.py
@@ -0,0 +1,24 @@
+movie_types_dict= {
+    "Action" : "13025",
+    "Comédie" : "13005",
+    "Aventure" : "13001",
+    "Animation" : "13026",
+    "Science-Fiction" : "13021",
+    "Thriller" : "13023",
+    "Romance" : "13024"
+}
+
+date_month_dict = {
+    'janvier' : 1,
+    'février' : 2,
+    "mars" : 3,
+    "avril" : 4,
+    "mai" : 5,
+    "juin" : 6,
+    "juillet" : 7,
+    "août" : 8,
+    "septembre" : 9,
+    "octobre" : 10,
+    "novembre" : 11,
+    "décembre" : 12
+}
